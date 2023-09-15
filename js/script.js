e214ae9a8a465981983dcf0eba0927c38bfb13cf
ManojRdk <140079622+ManojRdk@users.noreply.github.com>
2023-09-15 12:10:47 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+function scrollwindow(){
+    window.scrollBy(1800,900);
+ }
